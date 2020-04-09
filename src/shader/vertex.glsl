1c3f8c5630b5030b6187f57a1d5e6f7e881ba6c8
adkevin3307 <adkevin3307@gmail.com>
2020-04-09 18:32:21 +0800

    add location 2 to vertex shader

--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -1,6 +1,7 @@
 #version 440 core
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 normal;
+layout (location = 2) in vec2 texture;
 
 out float slice_check;
 out vec3 fragment_pos;
