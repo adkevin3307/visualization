2185c51aae053a45109b512de585e5c180e275b7
adkevin3307 <adkevin3307@gmail.com>
2020-04-17 17:44:00 +0800

    delete unused location

--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -1,7 +1,6 @@
 #version 440 core
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 normal;
-layout (location = 2) in vec2 texture;
 
 out float slice_check;
 out vec3 fragment_pos;
