f2485607acea2d8d6ead95612c172bceef230815
adkevin3307 <adkevin3307@gmail.com>
2020-06-16 20:48:42 +0800

    add kmeans

--- include/SammonMapping.h
+++ include/SammonMapping.h
@@ -13,11 +13,13 @@ class SammonMapping : public Method {
     using super = Method;
 
 private:
+    int group;
     vector<int> label;
     vector<vector<float>> data, _distance;
     vector<GLfloat> _vertex;
 
     float norm_2(vector<float> p1, vector<float> p2);
+    void kmeans(int group);
     void load_data();
     glm::vec2 descent(float lambda, glm::ivec2 index, vector<glm::vec2> &mapping_point);
 
